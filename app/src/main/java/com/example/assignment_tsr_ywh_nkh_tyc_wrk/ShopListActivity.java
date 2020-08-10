6c6d2c4eedc8dfb6697412e01d73e89bbcf65635
TangYikChin <66994561+TangYikChin@users.noreply.github.com>
2020-08-10 23:59:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/assignment_tsr_ywh_nkh_tyc_wrk/ShopListActivity.java
@@ -0,0 +1,70 @@
+package com.example.assignment_tsr_ywh_nkh_tyc_wrk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShopListActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_list);
+
+        spinner = findViewById(R.id.product_list);
+
+        List<String> product=new ArrayList<>();
+        product.add(0,"Product List");
+        product.add("Limited Product");
+        product.add("Clothed");
+        product.add("Health Care");
+        product.add("Electronic Device");
+
+        ArrayAdapter<String> dataAdapter;
+        dataAdapter=new ArrayAdapter(this,android.R.layout.simple_spinner_item,product);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        spinner.setAdapter(dataAdapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(parent.getItemAtPosition(position).equals("Product List"))
+                {
+
+                }
+                else {
+                    if (parent.getItemAtPosition(position).equals("Limited Product")) {
+                        Intent intent = new Intent(ShopListActivity.this, limited_class.class);
+                        startActivity(intent);
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/assignment_tsr_ywh_nkh_tyc_wrk/limited_class.java
@@ -0,0 +1,12 @@
+package com.example.assignment_tsr_ywh_nkh_tyc_wrk;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class limited_class extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.limited_product);
+    }
+}
