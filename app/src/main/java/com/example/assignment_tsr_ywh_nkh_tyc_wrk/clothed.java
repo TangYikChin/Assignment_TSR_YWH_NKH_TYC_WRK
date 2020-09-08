a60e3f8dbe65919a4237d8f7302a7de6cd5ec6db
Ahhong1129 <yowwh-wm19@student.tarc.edu.my>
2020-09-09 02:01:55 +0800

    commit by ahhong

--- app/src/main/java/com/example/assignment_tsr_ywh_nkh_tyc_wrk/clothed.java
+++ app/src/main/java/com/example/assignment_tsr_ywh_nkh_tyc_wrk/clothed.java
@@ -1,4 +1,26 @@
 package com.example.assignment_tsr_ywh_nkh_tyc_wrk;
 
-public class clothed {
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
+import android.media.Rating;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class clothed extends AppCompatActivity {
+
+    ImageView M,L,XL;
+    RatingBar ratingBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clothed);
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingbar);
+        LayerDrawable stars = (LayerDrawable)ratingBar.getProgressDrawable();
+
+        stars.getDrawable(2).setColorFilter(getResources().getColor(R.color.yellow), PorterDuff.Mode.SRC_ATOP);
+    }
 }
