ed37294e974d639f17a2be469b401629d9873c89
NG KIM HOW <ngkh-wm19@student.tar.edu.my>
2020-09-14 04:19:27 +0800

    commit by nkh

---
+++ app/src/main/java/com/example/assignment_tsr_ywh_nkh_tyc_wrk/nintendo_switch_v2.java
@@ -0,0 +1,14 @@
+package com.example.assignment_tsr_ywh_nkh_tyc_wrk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nintendo_switch_v2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nintendo_switch_v2);
+    }
+}
---
+++ app/src/main/java/com/example/assignment_tsr_ywh_nkh_tyc_wrk/xiaomi.java
@@ -0,0 +1,14 @@
+package com.example.assignment_tsr_ywh_nkh_tyc_wrk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class xiaomi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xiaomi);
+    }
+}
